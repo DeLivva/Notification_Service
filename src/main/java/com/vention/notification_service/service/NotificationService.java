9686e6a5fcd88250b65d460cb788759e50dd9e92
HasilbekR <asetrich01@gmail.com>
2023-12-15 00:19:13 +0500

    templates created for html files and only configuration for rabbitmq is left

--- src/main/java/com/vention/notification_service/service/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.vention.notification_service.service;
-
-import com.vention.notification_service.domain.Notification;
-
-public interface NotificationService {
-    Notification saveNotification(Notification notification);
-}
--- src/main/java/com/vention/notification_service/service/impl/MailSendingServiceImpl.java
+++ src/main/java/com/vention/notification_service/service/impl/MailSendingServiceImpl.java
@@ -1,6 +1,5 @@
 package com.vention.notification_service.service.impl;
 
-import com.vention.general.lib.exceptions.BadRequestException;
 import com.vention.notification_service.dto.ConfirmationTokenDto;
 import com.vention.notification_service.service.MailSendingService;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -12,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +20,7 @@ public class MailSendingServiceImpl implements MailSendingService {
 
     private final JavaMailSender mailSender;
     private static final Logger log = LoggerFactory.getLogger(MailSendingServiceImpl.class);
+    private final TemplateEngine templateEngine;
 
     @Override
     public void sendConfirmationToken(ConfirmationTokenDto tokenDto) {
@@ -33,7 +35,6 @@ public class MailSendingServiceImpl implements MailSendingService {
             mailSender.send(mailMessage);
         } catch (MessagingException e) {
             log.error("Error occurred while sending confirmation token: ", e);
-            throw new BadRequestException(e.getMessage());
         }
     }
 
@@ -42,27 +43,10 @@ public class MailSendingServiceImpl implements MailSendingService {
         String url = dotenv.get("CONFIRMATION_API_URL");
         return url + "?token=" + tokenDto.getToken();
     }
-
     private String createConfirmationMessage(String link) {
-        return "<html><body>" +
-                "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <title>Email Confirmation</title>\n" +
-                "</head>\n" +
-                "<body style=\"font-family: Arial, sans-serif;\">\n" +
-                "<div style=\"max-width: 600px; margin: auto; padding: 20px; text-align: center; background-color: #f4f4f4; border-radius: 10px;\">\n" +
-                "    <h2 style=\"color: #333;\">Confirm your email</h2>\n" +
-                "    <p style=\"color: #666;\">Please click on the button to confirm your email:</p>\n" +
-                "    <a href=\"" + link + "\" style=\"text-decoration: none;\">\n" +
-                "        <button style=\"display: inline-block; padding: 10px 20px; font-size: 16px; background-color: #007bff; color: #fff; border: none; border-radius: 5px; cursor: pointer;\">Confirm</button>\n" +
-                "    </a>\n" +
-                "</div>\n" +
-                "</body>\n" +
-                "</html>" +
-                "</body></html>";
+        Context context = new Context();
+        context.setVariable("link", link);
+        return templateEngine.process("confirmation-token", context);
     }
 }
 
