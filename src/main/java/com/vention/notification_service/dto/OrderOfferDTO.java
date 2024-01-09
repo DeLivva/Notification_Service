34e5da84481473489fe66b4dfda7d8ca9a35c383
HasilbekR <asetrich01@gmail.com>
2024-01-09 14:56:32 +0500

    order offer template updated

--- src/main/java/com/vention/notification_service/dto/OrderOfferDTO.java
+++ src/main/java/com/vention/notification_service/dto/OrderOfferDTO.java
@@ -1,14 +1,16 @@
 package com.vention.notification_service.dto;
 
+import com.vention.general.lib.dto.response.UserResponseDTO;
 import lombok.Data;
 
-import java.util.Date;
+import java.sql.Date;
+
 
 @Data
 public class OrderOfferDTO {
     private Long id;
     private Long userId;
-    private String senderName;
+    private UserResponseDTO userSender;
     private String startLocation;
     private String finalLocation;
     private Date deliveryDate;
